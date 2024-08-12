e1f649010f7a1155cac1a4dc2ea2f9ffc8fe5f3a
Nothingness1312 <maoukarin@gmail.com>
2024-08-12 17:26:03 +0700

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function kirimLaporan() {
     var pelapor = document.getElementById("pelapor").value;
     var korban = document.getElementById("korban").value;
+    var pelaku = document.getElementById("pelaku").value;
     var saksi = document.getElementById("saksi").value;
     var tanggal = document.getElementById("tanggal").value;
     var lokasi = document.getElementById("lokasi").value;
-    var pelaku = document.getElementById("pelaku").value;
-    var jenis = document.getElementById("Jenis Bullying").value;
+    var jenis = document.getElementById("jenis").value; // Ambil nilai dari input "jenis"
 
     var pesan = "Pelapor: " + pelapor + "%0ANama Korban: " + korban + "%0ANama Pelaku: " + pelaku + 
                 "%0ASaksi Mata: " + saksi + "%0ATanggal: " + tanggal + "%0ALokasi: " + lokasi;
